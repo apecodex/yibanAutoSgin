c79b2e441f4f9f01980e4df9e0cbf4cbcf889f33
ApeCode <1473018671@qq.com>
2020-09-08 09:28:57 +0800

    Update yiban.py

--- yiban.py
+++ yiban.py
@@ -8,12 +8,13 @@
 """
 import json
 import re
+import time
+
 import requests
 import util
 
 
 class Yiban:
-
     CSRF = "9cd990dfd520072e0ee032c46df5f99e"
     COOKIES = {"csrf_token": CSRF}
     HEADERS = {"Origin": "https://c.uyiban.com", "User-Agent": "yiban"}
@@ -56,45 +57,60 @@ class Yiban:
         else:
             return response
 
-    def auto(self):
+    def auto(self) -> json:
         """
         登录验证
         :return:
         """
-        location = self.session.get("http://f.yiban.cn/iapp/index?act=iapp7463&v=" + self.access_token, allow_redirects=False).headers["Location"]
+        location = self.session.get("http://f.yiban.cn/iapp/index?act=iapp7463&v=" + self.access_token,
+                                    allow_redirects=False).headers["Location"]
         verifyRequest = re.findall(r"verify_request=(.*?)&", location)[0]
-        return self.request("https://api.uyiban.com/base/c/auth/yiban?verifyRequest="+verifyRequest+"&CSRF=" + self.CSRF, cookies=self.COOKIES)
+        return self.request(
+            "https://api.uyiban.com/base/c/auth/yiban?verifyRequest=" + verifyRequest + "&CSRF=" + self.CSRF,
+            cookies=self.COOKIES)
 
-    def getUncompletedList(self):
+    def getUncompletedList(self) -> json:
         """
         获取未完成的表单
         :return:
         """
-        response = self.request("https://api.uyiban.com/officeTask/client/index/uncompletedList?CSRF=" + self.CSRF, cookies=self.COOKIES)
+        response = self.request("https://api.uyiban.com/officeTask/client/index/uncompletedList?CSRF=" + self.CSRF,
+                                cookies=self.COOKIES)
         return response
 
-    def getCompletedList(self):
+    def getUncompletedListTime(self) -> json:
+        """
+        获取特定时间未完成的表单
+        :return:
+        """
+        response = self.request("https://api.uyiban.com/officeTask/client/index/uncompletedList?StartTime=2020-08-31 00:00&EndTime="+time.strftime("%Y-%m-%d 23:59:00", time.localtime(int(time.time())))+"&CSRF=" + self.CSRF,
+                                cookies=self.COOKIES)
+        return response
+
+    def getCompletedList(self) -> json:
         """
         获取已经完成的表单
         :return:
         """
-        return self.request("https://api.uyiban.com/officeTask/client/index/completedList?CSRF=" + self.CSRF, cookies=self.COOKIES)
+        return self.request("https://api.uyiban.com/officeTask/client/index/completedList?CSRF=" + self.CSRF,
+                            cookies=self.COOKIES)
 
-    def getDetail(self, taskId):
+    def getDetail(self, taskId) -> json:
         """
         获取表单WFId
         获取发布人信息,后面提交表单时要用到
         :param taskId:
         :return:
         """
-        response = self.request("https://api.uyiban.com/officeTask/client/index/detail?TaskId=" + taskId + "&CSRF=" + self.CSRF, cookies=self.COOKIES)
+        response = self.request(
+            "https://api.uyiban.com/officeTask/client/index/detail?TaskId=" + taskId + "&CSRF=" + self.CSRF,
+            cookies=self.COOKIES)
         self.WFId = response['data']['WFId']
         self.Title = response['data']["Title"]
         self.PubOrgName = response["data"]["PubOrgName"]
         self.PubPersonName = response["data"]["PubPersonName"]
         print("WFID: " + self.WFId)
         return response
-
 
     def getForm(self):
         """
@@ -103,7 +119,8 @@ class Yiban:
         :return:
         """
         form_data = {}
-        response = self.request("https://api.uyiban.com/workFlow/c/my/form/%s?CSRF=%s" % (self.WFId, self.CSRF), cookies=self.COOKIES)
+        response = self.request("https://api.uyiban.com/workFlow/c/my/form/%s?CSRF=%s" % (self.WFId, self.CSRF),
+                                cookies=self.COOKIES)
         for i in response["data"]["Form"]:
             if i["component"] == "Radio":
                 print(i["props"]["label"] + ": " + i["props"]["extra"])
@@ -118,17 +135,18 @@ class Yiban:
                 pass
         util.writerTaskFrom(form_data)
 
-    def getFormapi(self):
+    def getFormapi(self) -> json:
         """
         首次使用,需要创建提交表单用的数据
         此方法是用来创建表单数据的
         :return:
         """
         form_data = {}
-        response = self.request("https://api.uyiban.com/workFlow/c/my/form/%s?CSRF=%s" % (self.WFId, self.CSRF), cookies=self.COOKIES)
+        response = self.request("https://api.uyiban.com/workFlow/c/my/form/%s?CSRF=%s" % (self.WFId, self.CSRF),
+                                cookies=self.COOKIES)
         return response
 
-    def nightAttendance(self, reason):
+    def nightAttendance(self, reason) -> json:
         """
         晚点名签到
         :param reason: 坐标信息
@@ -139,10 +157,11 @@ class Yiban:
             "SignInfo": reason,
             "OutState": "1"
         }
-        response = self.request("https://api.uyiban.com/nightAttendance/student/index/signIn?CSRF=" + self.CSRF, method="post", params=params, cookies=self.COOKIES)
+        response = self.request("https://api.uyiban.com/nightAttendance/student/index/signIn?CSRF=" + self.CSRF,
+                                method="post", params=params, cookies=self.COOKIES)
         return response
 
-    def submitApply(self, data, extend):
+    def submitApply(self, data, extend) -> json:
         """
         提交表单
         :param data: 提交表单的参数
@@ -158,26 +177,32 @@ class Yiban:
             params=params,
             cookies=self.COOKIES)
 
-    def getShareUrl(self, initiateId):
-        return self.request("https://api.uyiban.com/workFlow/c/work/share?InitiateId=%s&CSRF=%s" % (initiateId, self.CSRF), cookies=self.COOKIES)
+    def getShareUrl(self, initiateId) -> json:
+        return self.request(
+            "https://api.uyiban.com/workFlow/c/work/share?InitiateId=%s&CSRF=%s" % (initiateId, self.CSRF),
+            cookies=self.COOKIES)
 
     def photoRequirements(self):
         """
         晚点名签到所需
         :return:
         """
-        return self.request(url="https://api.uyiban.com/nightAttendance/student/index/photoRequirements?CSRF=" + self.CSRF, cookies=self.COOKIES)
+        return self.request(
+            url="https://api.uyiban.com/nightAttendance/student/index/photoRequirements?CSRF=" + self.CSRF,
+            cookies=self.COOKIES)
 
     def deviceState(self):
         """
         晚点名签到所需
         :return:
         """
-        return self.request(url="https://api.uyiban.com/nightAttendance/student/index/deviceState?CSRF=" + self.CSRF, cookies=self.COOKIES)
+        return self.request(url="https://api.uyiban.com/nightAttendance/student/index/deviceState?CSRF=" + self.CSRF,
+                            cookies=self.COOKIES)
 
     def sginPostion(self):
         """
         晚点名签到所需
         :return:
         """
-        return self.request(url="https://api.uyiban.com/nightAttendance/student/index/signPosition?CSRF=" + self.CSRF, cookies=self.COOKIES)
+        return self.request(url="https://api.uyiban.com/nightAttendance/student/index/signPosition?CSRF=" + self.CSRF,
+                            cookies=self.COOKIES)
