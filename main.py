646119a12e06442e51cbbea21e248b1aa498b61f
ApeCode <1473018671@qq.com>
2020-09-16 14:24:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,15 +18,23 @@ login_times = 1
 email_str = ""
 account = json.loads(util.readAccount()[0])
 
+flag = False
+
 for ac in account:
-    yiban = Yiban(ac, account[ac])
+    yiban = Yiban(account[0], account[1])
     while login_times <= 5:
         try:
             login = yiban.login()
             if login is not None and login["response"] == "100":
-                print("登录成功! " + yiban.name)
-                auto = yiban.auto()
-                data_url = auto["data"].get("Data")
+                try:
+                    data_url = auto["data"].get("Data")
+                except AttributeError as a:
+                    login = yiban.login()
+                    auto = yiban.auto()
+                    try:
+                        data_url = auto["data"].get("Data")
+                    except:
+                        print("表单和签到提交失败,请手动提交")
                 if data_url is not None:  # 授权过期
                     result_html = yiban.session.get(url=data_url, headers=yiban.HEADERS,
                                                 cookies={"loginToken": yiban.access_token}).text
@@ -43,73 +51,79 @@ for ac in account:
                     else:
                         print("授权失败！")
                     auto = yiban.auto()
+                # print(auto)
                 if auto["code"] == 0:
                     allList = yiban.getUncompletedListTime()
-                    if (allList["code"] == 0 and allList["data"] != []):
-                        today_taskID = allList["data"][-1]["TaskId"]
-                        detail = yiban.getDetail(today_taskID)
-                        extend = {
-                            "TaskId": today_taskID,
-                            "title": "任务信息",
-                            "content": [
-                                {"label": "任务名称", "value": detail["data"]["Title"]},
-                                {"label": "发布机构", "value": detail["data"]["PubOrgName"]},
-                                {"label": "发布人", "value": detail["data"]["PubPersonName"]}
-                            ]
-                        }
-                        cform_data = util.readTaskForm()
-                        if cform_data == []:
-                            print("首次使用,你需要填写表单信息")
-                            print("填写完成之后,将使用此信息进行提交")
-                            print("请仔细填写......")
-                            yiban.getForm()
-                        form_data = util.readTaskForm()[0]
-                        reason = util.getReason()[0]
-                        yiban.photoRequirements()
-                        yiban.deviceState()
-                        yiban.sginPostion()
-                        night_s = yiban.nightAttendance(reason)
-                        sb = yiban.submitApply(form_data, extend)
-                        if sb["code"] == 0 and night_s["code"] == 0:
-                            print("签到和表单提交成功!")
-                            share_url = yiban.getShareUrl(sb["data"])["data"]["uri"]
-                        
-                            email_str = util.html_format(
-                                time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))),
-                                "签到和表单提交成功",
-                                share_url, json.loads(reason)["Address"])
-                        
-                            util.log(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(
-                                int(time.time()))) + " 签到和表单提交成功 url: " + share_url + "\n")
-                        
-                            util.send_mail(email_str)
-                        elif sb["code"] == 0 and night_s["code"] != 0:
-                            print("表单提交成功!但是签到没成功")
-                            share_url = yiban.getShareUrl(sb["data"])["data"]["uri"]
-                        
-                            email_str = util.html_format(
-                                time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))),
-                                "表单提交成功!但是签到没成功!",
-                                share_url, "签到没成功")
-                        
-                            util.log(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(
-                                int(time.time()))) + " 表单提交成功!但是签到没成功 url: " + share_url + "\n")
-                        
-                            util.send_mail(email_str)
-                        elif sb["code"] != 0 and night_s["code"] == 0:
-                            print("签到提交成功!但是表单没成功")
-                        
-                            email_str = util.html_format(
-                                time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))),
-                                "签到提交成功!但是表单没成功!",
-                                "表单没成功", json.loads(reason)["Address"])
-                        
-                            util.log(
-                                time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))) + " 签到提交成功!但是表单没成功")
-                            util.send_mail(email_str)
+                    if allList["code"] == 0 and allList["data"] != []:
+                        nowTime = util.GenerateNowTime();
+                        for nowTask in allList["data"]:
+                            if nowTask["StartTime"] == nowTime:
+                                today_taskID = nowTask["TaskId"]
+                                detail = yiban.getDetail(today_taskID)
+                                title  = nowTask["Title"]
+                                flag = True
+                                break
+                        if (flag):
+                            extend = {
+                                "TaskId": today_taskID,
+                                "title": "任务信息",
+                                "content": [
+                                    {"label": "任务名称", "value": detail["data"]["Title"]},
+                                    {"label": "发布机构", "value": detail["data"]["PubOrgName"]},
+                                    {"label": "发布人", "value": detail["data"]["PubPersonName"]}
+                                ]
+                            }
+
+                            form_data = util.readTaskForm()[0]
+                            reason = util.getReason()[0]
+                            yiban.photoRequirements()
+                            yiban.deviceState()
+                            yiban.sginPostion()
+                            night_s = yiban.nightAttendance(reason)
+                            sb = yiban.submitApply(form_data, extend)
+                            if sb["code"] == 0 and night_s["code"] == 0:
+                                print("签到和表单提交成功!")
+                                share_url = yiban.getShareUrl(sb["data"])["data"]["uri"]
+                            
+                                email_str = util.html_format(
+                                    time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))),
+                                    "签到和表单提交成功",
+                                    share_url, json.loads(reason)["Address"])
+                            
+                                util.log(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(
+                                    int(time.time()))) + " 签到和表单提交成功 url: " + share_url + "\n")
+                            
+                                util.send_mail(email_str)
+                            elif sb["code"] == 0 and night_s["code"] != 0:
+                                print("表单提交成功!但是签到没成功")
+                                share_url = yiban.getShareUrl(sb["data"])["data"]["uri"]
+                            
+                                email_str = util.html_format(
+                                    time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))),
+                                    "表单提交成功!但是签到没成功!",
+                                    share_url, "签到没成功")
+                            
+                                util.log(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(
+                                    int(time.time()))) + " 表单提交成功!但是签到没成功 url: " + share_url + "\n")
+                            
+                                util.send_mail(email_str)
+                            elif sb["code"] != 0 and night_s["code"] == 0:
+                                print("签到提交成功!但是表单没成功")
+                            
+                                email_str = util.html_format(
+                                    time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))),
+                                    "签到提交成功!但是表单没成功!",
+                                    "表单没成功", json.loads(reason)["Address"])
+                            
+                                util.log(
+                                    time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))) + " 签到提交成功!但是表单没成功")
+                                util.send_mail(email_str)
+                            else:
+                                print("都没有成功!")
+                                util.send_mail("提交失败!")
                         else:
-                            print("都没有成功!")
-                            util.send_mail("提交失败!")
+                            print("没有可提交的表单!")
+                            util.send_mail("没有可提交的表单!")
                     else:
                         print("没有可提交的表单!")
                         util.send_mail("没有可提交的表单!")
