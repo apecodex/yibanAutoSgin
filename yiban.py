a0ed1e1bf3da9faf8d45bb758bf9c8908b038c1a
apeocde <1473018671@qq.com>
2021-08-24 15:31:25 +0800

    更新登录方式

--- yiban.py
+++ yiban.py
@@ -8,16 +8,53 @@
 """
 import json
 import re
+import sys
 import time
-
-import requests
+import os
 import util
+import base64
+
+try:
+    import requests
+except ModuleNotFoundError:
+    print("缺少requests依赖！程序将尝试安装依赖！")
+    os.system("pip3 install requests -i https://pypi.tuna.tsinghua.edu.cn/simple")
+    os.execl(sys.executable, 'python3', __file__, *sys.argv)
+
+try:
+    from Crypto.Cipher import PKCS1_v1_5
+    from Crypto.PublicKey import RSA
+except ModuleNotFoundError:
+    print("缺少pycryptodome依赖！程序将尝试安装依赖！")
+    os.system("pip3 install pycryptodome -i https://pypi.tuna.tsinghua.edu.cn/simple")
+    os.execl(sys.executable, 'python3', __file__, *sys.argv)
+
+
+def encryptPassword(pwd):
+    # 密码加密
+    PUBLIC_KEY = '''-----BEGIN PUBLIC KEY-----
+        MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA6aTDM8BhCS8O0wlx2KzA
+        Ajffez4G4A/QSnn1ZDuvLRbKBHm0vVBtBhD03QUnnHXvqigsOOwr4onUeNljegIC
+        XC9h5exLFidQVB58MBjItMA81YVlZKBY9zth1neHeRTWlFTCx+WasvbS0HuYpF8+
+        KPl7LJPjtI4XAAOLBntQGnPwCX2Ff/LgwqkZbOrHHkN444iLmViCXxNUDUMUR9bP
+        A9/I5kwfyZ/mM5m8+IPhSXZ0f2uw1WLov1P4aeKkaaKCf5eL3n7/2vgq7kw2qSmR
+        AGBZzW45PsjOEvygXFOy2n7AXL9nHogDiMdbe4aY2VT70sl0ccc4uvVOvVBMinOp
+        d2rEpX0/8YE0dRXxukrM7i+r6lWy1lSKbP+0tQxQHNa/Cjg5W3uU+W9YmNUFc1w/
+        7QT4SZrnRBEo++Xf9D3YNaOCFZXhy63IpY4eTQCJFQcXdnRbTXEdC3CtWNd7SV/h
+        mfJYekb3GEV+10xLOvpe/+tCTeCDpFDJP6UuzLXBBADL2oV3D56hYlOlscjBokNU
+        AYYlWgfwA91NjDsWW9mwapm/eLs4FNyH0JcMFTWH9dnl8B7PCUra/Lg/IVv6HkFE
+        uCL7hVXGMbw2BZuCIC2VG1ZQ6QD64X8g5zL+HDsusQDbEJV2ZtojalTIjpxMksbR
+        ZRsH+P3+NNOZOEwUdjJUAx8CAwEAAQ==
+        -----END PUBLIC KEY-----'''
+    cipher = PKCS1_v1_5.new(RSA.importKey(PUBLIC_KEY))
+    cipher_text = base64.b64encode(cipher.encrypt(bytes(pwd, encoding="utf8")))
+    return cipher_text.decode("utf-8")
 
 
 class Yiban:
     CSRF = "64b5c616dc98779ee59733e63de00dd5"
     COOKIES = {"csrf_token": CSRF}
-    HEADERS = {"Origin": "https://c.uyiban.com", "User-Agent": "YiBan"}
+    HEADERS = {"Origin": "'https://m.yiban.cn", 'AppVersion': '5.0.1', "User-Agent": "YiBan/5.0.1"}
     EMAIL = {}
 
     def __init__(self, mobile, password):
@@ -41,13 +78,15 @@ class Yiban:
         """
         params = {
             "mobile": self.mobile,
-            "password": self.password,
-            "imei": "0",
+            "password": encryptPassword(self.password),
+            "ct": "2",
+            "identify": "0",
         }
         # 新的登录接口
-        response = self.request("https://mobile.yiban.cn/api/v3/passport/login", params=params, cookies=self.COOKIES)
+        response = self.request("https://mobile.yiban.cn/api/v4/passport/login", method="post", params=params,
+                                cookies=self.COOKIES)
         if response is not None and response["response"] == 100:
-            self.access_token = response["data"]["user"]["access_token"]
+            self.access_token = response["data"]["access_token"]
             self.HEADERS["Authorization"] = "Bearer " + self.access_token
             # 增加cookie
             self.COOKIES["loginToken"] = self.access_token
@@ -60,11 +99,15 @@ class Yiban:
         登录验证
         :return:
         """
-        location = self.session.get("http://f.yiban.cn/iapp7463?access_token=" + self.access_token + "&v_time=" + str(int(round(time.time() * 100000))),
-                                    allow_redirects=False, cookies=self.COOKIES)
-        # 二次重定向
-        act = self.session.get("https://f.yiban.cn/iapp/index?act=iapp7463", allow_redirects=False, cookies=self.COOKIES).headers["Location"]
+        act = self.session.get("https://f.yiban.cn/iapp/index?act=iapp7463", allow_redirects=False,
+                               cookies=self.COOKIES).headers["Location"]
         verifyRequest = re.findall(r"verify_request=(.*?)&", act)[0]
+        self.HEADERS.update({
+            'origin': 'https://app.uyiban.com',
+            'referer': 'https://app.uyiban.com/',
+            'Host': 'api.uyiban.com',
+            'user-agent': 'yiban'
+        })
         response = self.request(
             "https://api.uyiban.com/base/c/auth/yiban?verifyRequest=" + verifyRequest + "&CSRF=" + self.CSRF,
             cookies=self.COOKIES)
@@ -80,17 +123,14 @@ class Yiban:
                                 cookies=self.COOKIES)
         return response
 
-    def getUncompletedListTime(self) -> json:
+    def getUncompletedListTime(self, st, et) -> json:
         """
         获取特定时间未完成的表单
         :return:
         """
-
-        start_time = time.strftime("%Y-%m-%d 00:00")
-        end_time = time.strftime("%Y-%m-%d 23:59:00", time.localtime(int(time.time())))
-
-        response = self.request("https://api.uyiban.com/officeTask/client/index/uncompletedList?StartTime="+start_time+"&EndTime="+end_time+"&CSRF=" + self.CSRF,
-                                cookies=self.COOKIES)
+        response = self.request(
+            "https://api.uyiban.com/officeTask/client/index/uncompletedList?StartTime=" + st + "&EndTime=" + et + "&CSRF=" + self.CSRF,
+            cookies=self.COOKIES)
         return response
 
     def getCompletedList(self) -> json:
